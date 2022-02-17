b600eba7ff1630387ec52df10f6c36ab128e809f
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-02-17 11:23:14 +0900

    [build.sh] use set -ex

--- build.sh
+++ build.sh
@@ -1,47 +1,43 @@
 #!/bin/sh
-
-run() {
-   echo "$@"
-   $@ || exit 1
-}
+set -ex
 
 APK_TOOLS=http://dl-cdn.alpinelinux.org/alpine/v3.15/main/x86_64/apk-tools-static-2.12.7-r3.apk
 
 VERSION=${VERSION:-latest-stable}
 ARCH=${ARCH:-x86_64}
 
-run rm -rf build
-run mkdir -p build
+rm -rf build
+mkdir -p build
 
-run cd build
+cd build
 
-run wget -O apk-tools.tar.gz ${APK_TOOLS}
-run tar -xvzf apk-tools.tar.gz
+wget -O apk-tools.tar.gz ${APK_TOOLS}
+tar -xvzf apk-tools.tar.gz
 APK=sbin/apk.static
 
-run modprobe nbd max_part=1
+modprobe nbd max_part=1
 
-run qemu-img create -f qcow2 root.qcow2 8G
+qemu-img create -f qcow2 root.qcow2 8G
 
-run qemu-nbd --connect=/dev/nbd0 root.qcow2
+qemu-nbd --connect=/dev/nbd0 root.qcow2
 
-run sleep 1
+sleep 1
 
-run mkfs.ext4 /dev/nbd0
+mkfs.ext4 /dev/nbd0
 
-run mkdir -p rootfs
+mkdir -p rootfs
 
-run mount /dev/nbd0 rootfs
+mount /dev/nbd0 rootfs
 
-run ${APK} --arch ${ARCH} -X http://dl-cdn.alpinelinux.org/alpine/${VERSION}/main/ \
+${APK} --arch ${ARCH} -X http://dl-cdn.alpinelinux.org/alpine/${VERSION}/main/ \
    -X http://dl-cdn.alpinelinux.org/alpine/${VERSION}/community/ \
    -U --allow-untrusted --root rootfs --initdb add alpine-base linux-virt util-linux e2fsprogs docker qemu-guest-agent
 
-run cd rootfs/etc/init.d
-run ln -s agetty agetty.ttyS0
-run cd ../../..
+cd rootfs/etc/init.d
+ln -s agetty agetty.ttyS0
+cd ../../..
 
-run echo "
+echo "
 auto lo
 iface lo inet loopback
 
@@ -56,33 +52,33 @@ echo "virtio_console" >>rootfs/etc/modules
 
 echo "DOCKER_OPTS=\"-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --bip 172.19.0.1/16\"" >>rootfs/etc/conf.d/docker
 
-run cp ../dinv rootfs/etc/init.d/dinv
-run cp ../dinv-post rootfs/etc/init.d/dinv-post
+cp ../dinv rootfs/etc/init.d/dinv
+cp ../dinv-post rootfs/etc/init.d/dinv-post
 
-run chroot rootfs rc-update add agetty.ttyS0 default
-run chroot rootfs rc-update add docker default
-run chroot rootfs rc-update add dinv default
-run chroot rootfs rc-update add dinv-post default
-run chroot rootfs rc-update add qemu-guest-agent default
+chroot rootfs rc-update add agetty.ttyS0 default
+chroot rootfs rc-update add docker default
+chroot rootfs rc-update add dinv default
+chroot rootfs rc-update add dinv-post default
+chroot rootfs rc-update add qemu-guest-agent default
 
-run chroot rootfs rc-update add devfs sysinit
-run chroot rootfs rc-update add dmesg sysinit
-run chroot rootfs rc-update add mdev sysinit
+chroot rootfs rc-update add devfs sysinit
+chroot rootfs rc-update add dmesg sysinit
+chroot rootfs rc-update add mdev sysinit
 
-run chroot rootfs rc-update add hwclock boot
-run chroot rootfs rc-update add modules boot
-run chroot rootfs rc-update add sysctl boot
-run chroot rootfs rc-update add hostname boot
-run chroot rootfs rc-update add bootmisc boot
-run chroot rootfs rc-update add syslog boot
-run chroot rootfs rc-update add networking boot
+chroot rootfs rc-update add hwclock boot
+chroot rootfs rc-update add modules boot
+chroot rootfs rc-update add sysctl boot
+chroot rootfs rc-update add hostname boot
+chroot rootfs rc-update add bootmisc boot
+chroot rootfs rc-update add syslog boot
+chroot rootfs rc-update add networking boot
 
-run chroot rootfs rc-update add mount-ro shutdown
-run chroot rootfs rc-update add killprocs shutdown
-run chroot rootfs rc-update add savecache shutdown
+chroot rootfs rc-update add mount-ro shutdown
+chroot rootfs rc-update add killprocs shutdown
+chroot rootfs rc-update add savecache shutdown
 
-run cp rootfs/boot/vmlinuz-* vmlinuz
-run cp rootfs/boot/initramfs-* initrd.img
+cp rootfs/boot/vmlinuz-* vmlinuz
+cp rootfs/boot/initramfs-* initrd.img
 
-run umount rootfs
-run qemu-nbd --disconnect /dev/nbd0
+umount rootfs
+qemu-nbd --disconnect /dev/nbd0
